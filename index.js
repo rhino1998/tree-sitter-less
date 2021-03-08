4737282a6c4d578129b921fe0b3662bbf53d6ffa
Riley Wilburn <riley@headspin.io>
2021-03-08 11:34:03 -0500

    css -> less

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 try {
-  module.exports = require("./build/Release/tree_sitter_css_binding");
+  module.exports = require("./build/Release/tree_sitter_less_binding");
 } catch (error) {
   try {
-    module.exports = require("./build/Debug/tree_sitter_css_binding");
+    module.exports = require("./build/Debug/tree_sitter_less_binding");
   } catch (_) {
     throw error
   }
--- src/scanner.c
+++ src/scanner.c
@@ -5,13 +5,13 @@ enum TokenType {
   DESCENDANT_OP,
 };
 
-void *tree_sitter_css_external_scanner_create() { return NULL; }
-void tree_sitter_css_external_scanner_destroy(void *p) {}
-void tree_sitter_css_external_scanner_reset(void *p) {}
-unsigned tree_sitter_css_external_scanner_serialize(void *p, char *buffer) { return 0; }
-void tree_sitter_css_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
+void *tree_sitter_less_external_scanner_create() { return NULL; }
+void tree_sitter_less_external_scanner_destroy(void *p) {}
+void tree_sitter_less_external_scanner_reset(void *p) {}
+unsigned tree_sitter_less_external_scanner_serialize(void *p, char *buffer) { return 0; }
+void tree_sitter_less_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
-bool tree_sitter_css_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
+bool tree_sitter_less_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
   if (iswspace(lexer->lookahead)) {
     lexer->advance(lexer, true);
 
